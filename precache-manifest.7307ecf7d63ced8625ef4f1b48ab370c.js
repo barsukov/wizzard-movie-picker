7818eeb76d03ed42156db03cfc692329e8865164
Wowa Barsukov <pro.barsukov@gamil.com>
2019-12-12 18:10:57 -0700

    Updates

---
+++ precache-manifest.7307ecf7d63ced8625ef4f1b48ab370c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c478f76b555f9a3d1eee0804c2e41211",
+    "url": "/wizzard-movie-picker/index.html"
+  },
+  {
+    "revision": "aede7c02cc4d2824ce4f",
+    "url": "/wizzard-movie-picker/static/css/main.5f5c4438.chunk.css"
+  },
+  {
+    "revision": "239db0897a270d33f6e3",
+    "url": "/wizzard-movie-picker/static/js/0.abaa93a4.chunk.js"
+  },
+  {
+    "revision": "0fcd5abf7f26c8d34abb",
+    "url": "/wizzard-movie-picker/static/js/3.362b2bd5.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/wizzard-movie-picker/static/js/3.362b2bd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aede7c02cc4d2824ce4f",
+    "url": "/wizzard-movie-picker/static/js/main.a22fa5a6.chunk.js"
+  },
+  {
+    "revision": "ac1340655395acbbe8bf",
+    "url": "/wizzard-movie-picker/static/js/runtime-main.16c1a5ee.js"
+  }
+]);
